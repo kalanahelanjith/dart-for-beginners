8a97ea001e5d9bfb89e658bd95eedc5c85dc23ec
kalanahelanjith <kalanahelanjith@gmail.com>
2024-11-17 15:07:12 +0530

    dart6

---
+++ DART6.dart
@@ -0,0 +1,29 @@
+void main() {
+  
+  // check weather 10 is greater than 8 >>> print 10 is greater than 8
+  
+  if(10>8){
+    print("10 is greater than 8");
+  }
+  
+  bool isGreater = false;
+  if(isGreater == false) {
+    print("yes is Greater");
+  }
+  
+  
+  
+  int waterLevel = 20 ;
+  
+  if (waterLevel == 10){
+    print("Light the PINK BULB");
+  }
+  if (waterLevel == 20){
+    print("Light the BLUE BUIB");
+  }
+  if (waterLevel == 30){
+    print("Light the GREEN");
+  }
+  
+  
+}
