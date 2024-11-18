5ef4c0ab7f23a5ad733655e94a41f28eecd4320f
kalanahelanjith <kalanahelanjith@gmail.com>
2024-11-18 10:47:16 +0530

    dart7

---
+++ dart7.dart
@@ -0,0 +1,18 @@
+void main(){
+  
+  bool isWeekend = false;
+  String weather = "rainy";
+  
+  if (isWeekend == true){
+    if (weather == "sunny"){
+      print("Today is sunny day!");
+    }
+    if (weather == "rainy"){
+      print("Today is rainy day");
+    }
+    if (isWeekend == false){
+      print("Todat is not a weekend!");
+    }
+  }
+
+}
