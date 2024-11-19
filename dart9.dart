c256e23334bec385b3b088ba3f9e19663815bddf
kalanahelanjith <kalanahelanjith@gmail.com>
2024-11-19 18:09:52 +0530

    dart9

---
+++ dart9.dart
@@ -0,0 +1,15 @@
+void main() {
+  int marks = 18;
+
+  if (marks >= 75) {
+    print("A");
+  } else if (marks >= 65) {
+    print("B");
+  } else if (marks >= 55) {
+    print("C");
+  } else if (marks >= 45) {
+    print("s");
+  } else {
+    print("W");
+  }
+}
